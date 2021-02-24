86bcf5e04f3e0ac555715e436aa3671d244d725c
VernandoGames <30465911+VernandoGames@users.noreply.github.com>
2021-02-23 21:40:16 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
     pwa: {
         name: 'Better Roblox',
         themeColor: '#33b5e5',
+        msTileColor: '#282b30',
         iconPaths: {
             favicon32: 'static/icons/favicon-32x32.png',
             favicon16: 'static/icons/favicon-16x16.png',
@@ -14,7 +15,21 @@ module.exports = {
         },
         manifestOptions: {
             background_color: '#282b30',
-            display: 'minimal-ui'
+            display: 'minimal-ui',
+            icons: [
+                {
+                    src: './static/icons/android-chrome-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+                {
+                    src: './static/icons/android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any maskable',
+                },
+            ],
         },
 
         workboxPluginMode: 'InjectManifest',
