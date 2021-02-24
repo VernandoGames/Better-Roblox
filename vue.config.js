5ab4e41fd12e57249359034cbf7fce93c7cc49ab
VernandoGames <30465911+VernandoGames@users.noreply.github.com>
2021-02-23 21:30:13 -0600

    just use basic assets for testing yes

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,13 @@ module.exports = {
     pwa: {
         name: 'Better Roblox',
         themeColor: '#33b5e5',
+        iconPaths: {
+            favicon32: 'static/icons/favicon-32x32.png',
+            favicon16: 'static/icons/favicon-16x16.png',
+            appleTouchIcon: 'static/icons/apple-touch-icon.png',
+            maskIcon: 'static/icons/safari-pinned-tab.svg',
+            msTileImage: 'static/icons/mstile-150x150.png',
+        },
         manifestOptions: {
             background_color: '#282b30',
             display: 'minimal-ui'
