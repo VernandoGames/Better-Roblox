912be66e3f38bc034cec42778bf749be43cb9092
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-04 00:12:04 +0000

    Deploy VernandoGames/Better-Roblox to github.com/VernandoGames/Better-Roblox.git:gh-pages

---
+++ precache-manifest.ec12ff006a51fdac2288c015a47966c9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3131a0b80c77a4aada",
+    "url": "/Better-Roblox/css/app.a1659fa6.css"
+  },
+  {
+    "revision": "a49e89d88570128b03f3",
+    "url": "/Better-Roblox/css/chunk-vendors.eb78aeb9.css"
+  },
+  {
+    "revision": "e00968c8eb7e821114ccca2b99c60780",
+    "url": "/Better-Roblox/index.html"
+  },
+  {
+    "revision": "2e3131a0b80c77a4aada",
+    "url": "/Better-Roblox/js/app.300fc10e.js"
+  },
+  {
+    "revision": "a49e89d88570128b03f3",
+    "url": "/Better-Roblox/js/chunk-vendors.cf70a1c8.js"
+  },
+  {
+    "revision": "5d7d82fbfcf33f9a2415bbc01f644a37",
+    "url": "/Better-Roblox/manifest.json"
+  },
+  {
+    "revision": "f86efccd5b81b6a1c7cb4fe8e64e76a7",
+    "url": "/Better-Roblox/static/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "576b65fa89b9ab5210c61faa7f1876bb",
+    "url": "/Better-Roblox/static/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "e9ba652edcd52f119e482bb1db74cfdb",
+    "url": "/Better-Roblox/static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "a46d35c93037f254c55cdabede051983",
+    "url": "/Better-Roblox/static/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "83d525cb28ed6f071315ab37c15acacd",
+    "url": "/Better-Roblox/static/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "3f7b48d488a2a442f6fe3c87e9987281",
+    "url": "/Better-Roblox/static/icons/icon-128x128.png"
+  },
+  {
+    "revision": "7005dc094a534763c9c19ac10ba9ef9c",
+    "url": "/Better-Roblox/static/icons/icon-144x144.png"
+  },
+  {
+    "revision": "62297680d48b051c776cc03f76f35694",
+    "url": "/Better-Roblox/static/icons/icon-152x152.png"
+  },
+  {
+    "revision": "c470d15a57e090c1f577782319568ddb",
+    "url": "/Better-Roblox/static/icons/icon-192x192.png"
+  },
+  {
+    "revision": "8c7677c484782c05c46bebd1aaec98b5",
+    "url": "/Better-Roblox/static/icons/icon-384x384.png"
+  },
+  {
+    "revision": "757b980427859b0293c738139c8284bf",
+    "url": "/Better-Roblox/static/icons/icon-512x512.png"
+  },
+  {
+    "revision": "0a53e3bfc2ace1572edc1f7b682b2dac",
+    "url": "/Better-Roblox/static/icons/icon-72x72.png"
+  },
+  {
+    "revision": "9d98f5428a0bdc0dad96f2b645a07db7",
+    "url": "/Better-Roblox/static/icons/icon-96x96.png"
+  },
+  {
+    "revision": "40e17dfec48a5642d9303064f7d1bbe7",
+    "url": "/Better-Roblox/static/icons/mstile-144x144.png"
+  },
+  {
+    "revision": "c35b73d9b17f9742aab25a33dbf5fc02",
+    "url": "/Better-Roblox/static/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "57ad7de89e3afd90433b81759e16a630",
+    "url": "/Better-Roblox/static/icons/mstile-310x150.png"
+  },
+  {
+    "revision": "7980ff8004dcf3317b8338fc8f9c9a99",
+    "url": "/Better-Roblox/static/icons/mstile-310x310.png"
+  },
+  {
+    "revision": "93a40f5dae1c679507e46cce7b1e529c",
+    "url": "/Better-Roblox/static/icons/mstile-70x70.png"
+  },
+  {
+    "revision": "ebfe43e7806a1e46ad843e31ea504c18",
+    "url": "/Better-Roblox/static/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "1787600aa6afb88ce9ec7f0bf847b854",
+    "url": "/Better-Roblox/static/logo-square.png"
+  },
+  {
+    "revision": "13c0e5a64e6f5387c6ce5a1790b71b52",
+    "url": "/Better-Roblox/static/logo.svg"
+  }
+]);
