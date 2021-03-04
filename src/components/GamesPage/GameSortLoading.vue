f01825994c818f2acee6e86290ff1a57347505f6
Vern on Linux <30465911+VernandoGames@users.noreply.github.com>
2021-03-03 18:09:10 -0600

    loading templates

---
+++ src/components/GamesPage/GameSortLoading.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="sort-loading" class="game-sort">
+        <div id="title-bar">
+            <div class="loading-indicator" style="overflow: hidden">
+                <div class="loading-indicator loading-transition" style="height:20px">
+
+                </div>
+            </div>
+        </div>
+        <div class="sort-container">
+            <div class="horizontal-scroller">
+                <div class="horizontally-scrollable">
+                    <div v-for="(i) in 8" :key="i" class="inner-content">
+                        <div class="game-card-thumb-container" style="overflow: hidden;">
+                            <div class="loading-indicator" style="height:150px; width:150px">
+                                <div class="loading-indicator loading-transition">
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="game-card-title-loading">
+                            <div class="loading-indicator loading-transition">
+
+                            </div>
+                        </div>
+                        <div id="content-ratings-loading" style="display:flex; width:90%; margin: auto auto 0 auto;">
+                            <div class="game-card-title-loading" style="width: 45%">
+                                <div class="loading-indicator loading-transition"></div>
+                            </div>
+                            <div class="game-card-title-loading" style="width: 45%">
+                                <div class="loading-indicator loading-transition"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'game-sort-loading'
+}
+</script>
+
+<style lang="scss">
+
+    @import '../../styles/theming';
+
+</style>
--- src/components/pages/Games.vue
+++ src/components/pages/Games.vue
@@ -1,24 +1,36 @@
 <template>
     <div id='games-page'>
         
-        <div v-for="(sort, i) in sorts" :key="i">
-            <game-sort :displayName="sort.displayName" :token="sort.token" :name="sort.name" :targetId="sort.gameSetTargetId" :pageId="context.pageId"/>
+        <div v-if="loaded">
+            <div v-for="(sort, i) in sorts" :key="i">
+                <game-sort :displayName="sort.displayName" :token="sort.token" :name="sort.name" :targetId="sort.gameSetTargetId" :pageId="context.pageId"/>
+            </div>
+        </div>
+        <div v-else>
+            <div v-for="(i) in 8" :key="i">
+                <game-sort-loading/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import GamesSource from '../../data/GamesSource';
+import loadingSort from '../GamesPage/GameSortLoading.vue';
 
 export default {
     name: 'games', 
     props: ['darkMode'],
     params: ['id'],
+    components: {
+        'game-sort-loading': loadingSort
+    },
 
     data() {
         return {
             sorts: [],
             context: [],
+            loaded: false,
         }
     },
 
@@ -69,6 +81,142 @@ export default {
     #app.dark #games-page {
         background: $color-content-bg-dark;
         color: $color-content-text-dark;
+    }
+
+    @-webkit-keyframes loading {
+        0% {
+            left: -50%;
+            width: 50%;
+        }
+        100% {
+            left: 100%;
+            width: 30%;
+        }
+    }
+
+    @keyframes loading {
+        0% {
+            left: -50%;
+            width: 50%;
+        }
+        100% {
+            left: 100%;
+            width: 30%;
+        }
+    }
+
+    #game-sort {
+        background: $color-content-bg;
+        min-height: 160px;
+        height: 260px;
+        align-content: center;
+        float: none;
+        margin: auto;
+        width: calc(100% - 100px);
+    }
+
+    #app.dark #game-sort {
+        background: $color-content-bg-dark;
+        color: $color-content-text-dark;
+    }
+
+    .sort-container {
+        overflow: hidden;
+        width: calc(100% - 48px);
+        margin: 0px;
+    }
+
+    .inner-content {
+        flex-shrink: 0;
+        align-items: center;
+        justify-content: center;
+        width: 164px;
+        height: calc(100% - 10px);
+        border: transparent;
+        border-radius: 5px;
+    }
+
+
+    #game-cards {
+        white-space: nowrap;
+    }
+
+    .game-cards .game-card {
+        float: none;
+        width: 164px;
+        height: 240px;
+        padding: 0 14px 0 0;
+        display: inline-block;
+        margin-bottom: 12px;
+        vertical-align: top;
+        white-space: normal;
+    }
+
+    .horizontally-scrollable {
+        transition: left .5s ease;
+        float: left;
+        min-height: 270px;
+        position: relative;
+        white-space: nowrap;
+    }
+
+    .horizontally-scrollable {
+        display: flex;
+    }
+
+    .loading-indicator {
+        background: darken($color-content-bg, 5%);
+        margin: auto;
+        border-radius: 12px;
+    }
+
+    #app.dark .loading-indicator {
+        background: lighten($color-content-bg-dark, 5%);
+    }
+
+    .loading-transition {
+        background: darken($color-content-bg, 5%);
+        position: relative;
+        width: 40%;
+        height: 100%;
+        z-index: 1;
+    }
+
+    #app.dark .loading-transition {
+        background: lighten($color-content-bg-dark, 5%);
+    }
+
+    #title-bar {
+        height: 25px;
+        width: 300px;
+        margin: auto auto auto 20px;
+        overflow: hidden;
+    }
+
+    .game-card-title-loading {
+        background: darken($color-content-bg, 5%);
+        height: 1.4em;
+        width: 90%;
+        border-radius: 8px;
+        margin: 5px auto 0 auto;
+        overflow: hidden;
+    }
+
+    #app.dark .game-card-title-loading {
+        background: lighten($color-content-bg-dark, 5%);
+    }
+
+    .loading-indicator .loading-transition {
+        margin: auto;
+        overflow: hidden;
+    }
+
+    .loading-transition {
+        height: 100%;
+    }
+
+    .game-card-thumb-container {
+        margin: 0 auto 0 auto;
     }
 
 </style>
